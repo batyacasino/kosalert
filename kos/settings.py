94db54ce89ff2fc0f7181b2c49e1959788d09321
batyacasino <51843016+batyacasino@users.noreply.github.com>
2020-08-24 17:43:29 +0300

    Update settings.py

--- kos/settings.py
+++ kos/settings.py
@@ -13,12 +13,8 @@ ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'post_db',
-        'USER': 'asist',
-        'PASSWORD': '929227007Asdec',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
