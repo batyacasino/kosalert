4641f0fc398e9a42b77a85078aa44db160374dad
batyacasino <51843016+batyacasino@users.noreply.github.com>
2020-08-24 17:31:20 +0300

    Update settings.py

--- kos/settings.py
+++ kos/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 #SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
 DEBUG = False
 
-ALLOWED_HOSTS = ['.heroku.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 DATABASES = {
     'default': {
